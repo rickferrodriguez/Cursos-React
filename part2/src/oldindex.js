c0c89d2e5028d4d014930bfa2ef316fbff3f1d6d
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-02-22 22:07:46 -0500

    actualizando el estado, con el uso de componentes y las props

--- part2/src/oldindex.js
+++ part2/src/oldindex.js
@@ -3,16 +3,49 @@ import {useState} from 'react'
 import React from 'react';
 // import App from './App';
 
+// La aplicación se renderiza cuando llegan nuevas props, pero esto no 
+// significa que el valor va ser el mismo, puede llegar un numero por 
+// defecto pero no es el mismo del principio
+function Counter ({number}) {
+    return (
+        <p>{number}</p>
+    )
+}
 
 function OldIndex () {
-    const [countValue, countUpdate] = useState(0)
+    // Destructuración del use state, que devuelve un array con dos posiciones,
+    // el primero es el valor actual y el segundo devuelve una función con
+    // el valor que le deseamos cambiar
+    const [countValue, updateValue] = useState(0)
     // const contador = useState(13)
     // const countValue = contador[0]
     // const countUpdate = contador[1]
+
+    const handleClick = () => {
+        // updateValue(countValue + 1)
+        updateValue((prevValue) => {
+            // con esto nos encargamos de revisar el valor anterior para 
+            // actualizar el nuevo, es más seguro
+            return prevValue + 1
+        })
+    }
+
+    const handleClickReset = () => {
+        return updateValue(0)
+    }
+
+    const isEven = countValue % 2 === 0
+    // Renderizado condicional (a partir de una cosa poder mostrar una u otra cosa)
+    const messageEven = isEven ? 'Es par' : 'Es impar'
+
+    // En la medida de lo posible usar un 'onClick' para capturar eventos
     return (
         <div>
-        <p>El valor del contador es:</p>
-        <p>{countValue}</p>
+            <p>El valor del contador es:</p>
+            <Counter number={countValue} />
+            <button onClick={handleClick}>Increment</button>
+            <button onClick={handleClickReset}>Reset</button>
+            <p>Este Valor es: {messageEven} </p>
         </div>
     )
 }
