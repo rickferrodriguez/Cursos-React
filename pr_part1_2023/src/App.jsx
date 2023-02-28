706964e086b34e3f733310e82c7311863daa335f
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-02-28 14:57:23 -0500

    using the array to render each user

--- pr_part1_2023/src/App.jsx
+++ pr_part1_2023/src/App.jsx
@@ -1,24 +1,38 @@
 import TwitterCard from './twitterCard.jsx'
 import './App.css'
 
+const users = [
+  {
+    nombre: 'Miguel Angel Durán',
+    userName: 'midudev',
+    isFollowing: true
+  },
+  {
+    nombre: 'Ibai Llanos',
+    userName: 'IbaiLlanos',
+    isFollowing : false
+  }
+]
+
 function App() {
 
   return (
     <section className="section">
       <h3>You might like</h3>
-      <TwitterCard 
-        userName='midudev' 
-        initialIsFollowing = {false}
-      >
-        Miguel Angel Durán
-      </TwitterCard>
-
-      <TwitterCard 
-        userName='IbaiLlanos' 
-        initialIsFollowing 
-      >
-        Ibai Llanos
-      </TwitterCard>
+      {
+        users.map((user) => {
+          const {nombre, userName, isFollowing} = user
+           return (
+            <TwitterCard
+              key={userName}
+              userName={userName}
+              initialIsFollowing={isFollowing}
+            >
+              {nombre}
+            </TwitterCard>
+           ) 
+        })
+      }
     </section>
   )
 }
