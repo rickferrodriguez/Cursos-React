60bc4cef3c8ab99f6e3bda9f4cee32c971be2518
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-03-03 13:00:30 -0500

    tic tac toe finished

--- 02-Tic-dos/src/App.jsx
+++ 02-Tic-dos/src/App.jsx
@@ -7,6 +7,17 @@ const TURNS = {
   O: 'o'
 }
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
 function Square ({index,isSelected ,updateBoard, children}) {
   const className = `square ${isSelected ? 'is-selected' : ''}`
 
@@ -23,16 +34,47 @@ function App() {
 
   const [board, setBoard] = useState(Array(9).fill(null))
   const [turn, setTurn] = useState(TURNS.X)
+  const [winner,setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+    // revisamos todas las combinaciones ganadoras
+    for (const combo of WINNER_COMBOS){
+      const [a,b,c] = combo
+      if(
+      boardToCheck[a] && // 0 -> x u o
+      boardToCheck[a] === boardToCheck[b] &&
+      boardToCheck[a] === boardToCheck[c]
+      ){
+        return boardToCheck[a]
+      }
+    }
+    return null
+  }
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
 
   const updateBoard = (index) => {
+    // no renderizar el square si ya existe algo
+    if(board[index] || winner) return
+
+    //actualizamos el board
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
 
+    // actualizamos el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
 
-
+    // revisar si hay ganador
+    const newWinner = checkWinner(newBoard)
+    if(newWinner) {
+      setWinner(newWinner)
+    }
 
   }
 
@@ -62,6 +104,28 @@ function App() {
           {TURNS.O}
         </Square>
       </section>
+        {
+          winner !== null && (
+          <section className='winner'>
+            <div className="text">
+                <h2>
+                  {
+                    winner === false
+                    ? 'empate'
+                    : 'ganó'
+                  }
+                </h2>
+                <header className="win">
+                  {winner && <Square>{winner}</Square>}
+                </header>
+
+                <footer>
+                  <button onClick={resetGame}>Empezar de Nuevo</button>
+                </footer>
+              </div>
+          </section>
+          )
+        }
     </main>
   )
 }
