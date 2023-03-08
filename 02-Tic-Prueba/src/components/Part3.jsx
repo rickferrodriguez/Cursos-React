f318023d5e6c4446f6f912cc919bcf059917ea5c
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-03-07 22:40:48 -0500

    setting up the board and the turn states

--- 02-Tic-Prueba/src/components/Part3.jsx
+++ 02-Tic-Prueba/src/components/Part3.jsx
@@ -1,8 +1,14 @@
 import { TURNS } from '../constants.js'
 import { useState} from 'react'
-const Square = ({children}) => {
+const Square = ({children, index, isSelected, updateTurn}) => {
+  const className = `square ${isSelected ? 'is-selected': ''}`
+  const handleClick = () => {
+    updateTurn(index)
+  }
   return (
-    <div className="square">
+    <div 
+      onClick={handleClick}
+      className={className}>
       {children}
     </div>
   )
@@ -10,15 +16,37 @@ const Square = ({children}) => {
 
 export function Part3 () {
   const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.X)
+
+  const updateTurn = (index) => {
+    if(board[index]) return
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+  }
   return (
     <section className="board">
       <div className="game">
         {
           board.map((square, index) => (
-            <Square> {index}</Square>
+            <Square 
+              key={index}
+              index={index}
+              updateTurn={updateTurn}
+            > 
+              {square}
+            </Square>
           ))
         }
       </div>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
     </section>
   )
 }
