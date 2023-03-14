b7be24f7465fd5a26b1a817a2a64cb4be9359e50
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-03-13 23:50:45 -0500

    update repo with third project

--- monorepo/projects/03-mouse/src/App.jsx
+++ monorepo/projects/03-mouse/src/App.jsx
@@ -1,7 +1,62 @@
+import { useEffect, useState } from 'react'
+
+const FollowMouse = () => {
+  const [enable, setEnable] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  const handleClick = () => {
+    return setEnable(!enable)
+  }
+
+  useEffect(() => {
+    console.log('efecto', { enable })
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      console.log('handleMove', { clientX, clientY })
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enable) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      console.log('cleanup')
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enable])
+
+  return (
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px , ${position.y}px)`
+      }}
+      />
+      <button onClick={handleClick}>
+        {enable
+          ? 'Desactivar seguir puntero'
+          : 'Activar seguir puntero'}
+      </button>
+    </main>
+  )
+}
 
 function App () {
   return (
-    <h3>Proyecto 03</h3>
+    <main>
+      <FollowMouse />
+    </main>
   )
 }
 
