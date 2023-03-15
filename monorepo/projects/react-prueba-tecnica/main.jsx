91cc72adc0bc20c445c1836d7a6d6c4500d5c49f
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-03-15 15:01:26 -0500

    haciendo fetch con un estado que depende de otro

--- monorepo/projects/react-prueba-tecnica/main.jsx
+++ monorepo/projects/react-prueba-tecnica/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { App } from './src/App.jsx'
+import './App.css'
 
 const root = createRoot(document.getElementById('app'))
-root.render(
-  <App />
-)
+
+root.render(<App />)
--- monorepo/projects/react-prueba-tecnica/src/App.jsx
+++ monorepo/projects/react-prueba-tecnica/src/App.jsx
@@ -1,21 +1,46 @@
-import {useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
+import { getRandomFact } from './services/facts.js'
 
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
-const CAT_ENDPONIT_IMAGE_URL = `https://cataas.com/cat/says/${message}?size=50&color=red&json=true`
+// const CAT_ENDPONIT_IMAGE_URL = `https://cataas.com/cat/says/${message}?size=50&color=red&json=true`
+const CAT_PREFIX_URL = 'https://cataas.com'
 
 export function App () {
-  const [fact, setFact] = useState('lorem')
+  const [fact, setFact] = useState()
+  const [imgUrl, setImgUrl] = useState()
 
+  // recuperar la cita al cargar la página
   useEffect(() => {
-    fetch('https://catfact.ninja/fact')
-    .then(res => res.json())
-    .then(data => setFact(data.fetch))
+    getRandomFact().then(newFact => setFact(newFact))
   }, [])
+
+  // para cargar la imagen cada vez que tenemos una cita nueva
+  useEffect(() => {
+    if (!fact) return
+    // first three words study join, slice, split
+    // const threeWords = fact.split(' ').slice(0, 3).join(' ')
+    const threeFirstWords = fact.split(' ', 3).join(' ')
+
+    fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
+      .then(res => res.json())
+      .then(response => {
+        const { url } = response
+        setImgUrl(url)
+      })
+  }, [fact])
+
+  const handleClick = async () => {
+    const newFact = await getRandomFact()
+    setFact(newFact)
+  }
 
   return (
     <main>
       <h1>App de gatitos</h1>
-      <p>{fact}</p>
+      <button onClick={handleClick}>Get new Fact</button>
+      <article className='card-cat'>
+        <p>{fact}</p>
+        {imgUrl && <img className='img-card' src={`${CAT_PREFIX_URL}${imgUrl}`} alt={`Image extracted using the first rhee words for ${fact}`} />}
+      </article>
     </main>
   )
 }
