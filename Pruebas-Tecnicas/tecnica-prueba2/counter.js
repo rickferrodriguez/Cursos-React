59ddf0085b519d2fb75bdb2a2cc834c93b26ce55
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-03-18 16:53:55 -0500

    class review class three, creating the entry point

---
+++ Pruebas-Tecnicas/tecnica-prueba2/counter.js
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom'
+
+const root = createRoot(document.getElementById('app'))
+
+root.render(
+  <h1>hola desde render </h1>
+)
---
+++ Pruebas-Tecnicas/tecnica-prueba2/main.jsx
@@ -0,0 +1,5 @@
+import { createRoot } from 'react-dom'
+
+const root = createRoot(document.getElementById('app'))
+
+root.render(<h1>hola desde app</h1>)
