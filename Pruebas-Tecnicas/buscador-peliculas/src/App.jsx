4fd0b75ae641b1a6002c563121052fb03b7f08bf
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-03-19 18:25:36 -0500

    new component listOfMovies

--- Pruebas-Tecnicas/buscador-peliculas/src/App.jsx
+++ Pruebas-Tecnicas/buscador-peliculas/src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import responseMovies from './mocks/with-results.json'
+import { ListOfMovies } from './components/Movies.jsx'
+// import withOutResults from './mocks/no-results.json'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App () {
+  const movies = responseMovies.Search
+  const hasMovies = movies?.length > 0
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='page'>
+      <header>
+        <h1>Buscador de películas</h1>
+        <form className='form'>
+          <input type='text' placeholder='Avengers, StarWars, The Matrix, ...' />
+          <button type='submit'>Buscar</button>
+        </form>
+      </header>
+
+      <main>
+        {
+          hasMovies
+            ? <ListOfMovies movies={movies} />
+            : (
+              <p>no se encotraron peliculas</p>
+              )
+        }
+      </main>
     </div>
   )
 }
---
+++ Pruebas-Tecnicas/buscador-peliculas/src/components/Movies.jsx
@@ -0,0 +1,15 @@
+export function ListOfMovies ({ movies }) {
+  return (
+    <ul>
+      {
+        movies.map(movie => (
+          <li key={movie.imdbID}>
+            <h3>{movie.Title}</h3>
+            <p>{movie.Year}</p>
+            <img src={movie.Poster} alt={movie.Title} />
+          </li>
+        ))
+      }
+    </ul>
+  )
+}
