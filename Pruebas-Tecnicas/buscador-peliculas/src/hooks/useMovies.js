83b6798b61d8ca00b0ee58979eff8d1012a588b3
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-03-20 23:22:09 -0500

    fetch directo

--- Pruebas-Tecnicas/buscador-peliculas/src/hooks/useMovies.js
+++ Pruebas-Tecnicas/buscador-peliculas/src/hooks/useMovies.js
@@ -1,6 +1,8 @@
 import { useState } from 'react'
-import withResults from '../mocks/with-results.json'
+// import withResults from '../mocks/with-results.json'
 import withOutResults from '../mocks/no-results.json'
+
+const PELI_ENDPOINT = 'http://www.omdbapi.com/?apikey=5a03f14a'
 
 export function useMovies ({ search }) {
   const [responseMovies, setResponseMovies] = useState([])
@@ -16,7 +18,12 @@ export function useMovies ({ search }) {
 
   const getMovies = () => {
     if (search) {
-      setResponseMovies(withResults)
+      // setResponseMovies(getMoviesFromName)
+      fetch(`${PELI_ENDPOINT}&s=${search}`)
+        .then(res => res.json())
+        .then(data => {
+          setResponseMovies(data)
+        })
     } else {
       setResponseMovies(withOutResults)
     }
