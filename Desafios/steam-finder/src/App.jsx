22c7b386471131d0b0fbfdc09a60db636e2f03f5
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-04-04 15:35:30 -0500

    Listed steam games

--- Desafios/steam-finder/src/App.jsx
+++ Desafios/steam-finder/src/App.jsx
@@ -4,12 +4,15 @@ function App () {
   const hasGames = withResults?.length > 0
   const mappedGames = withResults?.map(game => ({
     id: game.appId,
-    title: game.title
+    title: game.title,
+    image: game.imgUrl,
+    url: game.url,
+    price: game.price
   }))
   return (
-    <div className='w-full p-4 gap-4 flex flex-col items-center'>
-      <h1 className='text-2xl font-bold'>STEAM FINDER</h1>
-      <form className='flex gap-4'>
+    <div className='w-full py-4 gap-4 flex flex-col p-2'>
+      <h1 className='text-2xl font-bold text-center'>STEAM FINDER</h1>
+      <form className='flex gap-4 justify-center'>
         <input
           className='rounded'
           type='text'
@@ -23,11 +26,20 @@ function App () {
       <main>
         {
         hasGames && (
-          <ul>
+          <ul className='flex flex-col gap-1 list-none bg-sky-950 text-gray-200'>
             {
             mappedGames.map(game => (
-              <li key={game.id}>
-                <p>{game.title}</p>
+              <li
+                key={game.id}
+                className='border-gray-400 border-t-[1px]  px-2'
+              >
+                <a href='' className='flex gap-2  py-2'>
+                  <img src={game.image} alt='' />
+                  <section>
+                    <h2 className='text-base'>{game.title}</h2>
+                    <p>{game.price}</p>
+                  </section>
+                </a>
               </li>
             ))
             }
