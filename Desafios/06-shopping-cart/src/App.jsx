3129972dc32c5212f187833681a62d864b5edaf6
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-04-09 17:27:05 -0500

    useContext global state

--- Desafios/06-shopping-cart/src/App.jsx
+++ Desafios/06-shopping-cart/src/App.jsx
@@ -1,24 +1,12 @@
 import { useState } from 'react'
 import { Header } from './components/Header'
 import { Products } from './components/Products'
+import { useFilters } from './hooks/useFilters'
 import { products as initialProducts } from './mocks/withResults.json'
 
 function App () {
   const [products, setProducts] = useState(initialProducts)
-  const [filters, setFilters] = useState({
-    category: 'all',
-    minPrice: 0
-  })
-
-  const filterProducts = ({ products }) => {
-    return products.filter((product) => {
-      return product.price >= filters.minPrice && (
-        filters.category === 'all' ||
-        filters.category === product.category
-      )
-    })
-  }
-
+  const { filterProducts, setFilters } = useFilters()
   const filteredProducts = filterProducts({ products })
 
   return (
