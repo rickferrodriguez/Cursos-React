d90205afb1aa828a3e256a8404c4ede819386862
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-04-10 08:02:07 -0500

    shopping cart review 3

--- Desafios/06-shopping-cart/src/App.jsx
+++ Desafios/06-shopping-cart/src/App.jsx
@@ -1,21 +1,9 @@
-import { Cart } from './components/Cart'
-import { Header } from './components/Header'
-import { Products } from './components/Products'
-import { CartProvider } from './context/cart'
-import { useFilters } from './hooks/useFilters'
-import { products as initialProducts } from './mocks/withResults.json'
 
 function App () {
   // const [products, setProducts] = useState(initialProducts)
-  const { filterProducts } = useFilters()
-  const filteredProducts = filterProducts(initialProducts)
 
   return (
-    <CartProvider>
-      <Header />
-      <Cart />
-      <Products products={filteredProducts} />
-    </CartProvider>
+    <h1>hola desde App</h1>
   )
 }
 
