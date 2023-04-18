dd6eb80bd6f55ba63bbd1434fcd105b949a568d5
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-04-18 16:05:07 -0500

    reboot with tailwind

--- Desafios/review-shopping/src/components/Header.jsx
+++ Desafios/review-shopping/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Filters } from './Filters'
 export function Header () {
   return (
     <header>
-      <h1>Shopping Cart</h1>
+      <h1 className='text-sky-600'>Shopping Cart</h1>
       <Filters />
     </header>
   )
