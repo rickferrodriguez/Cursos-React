0625b30d74679033a3749679aec8878ffd18766a
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-04-26 21:47:48 -0500

    remapping the API contract

--- Desafios/final-review/src/components/Products.jsx
+++ Desafios/final-review/src/components/Products.jsx
@@ -1,16 +1,17 @@
 export function ProductItem ({ product }) {
+  const { title, image, price } = product
   return (
     <li className='bg-sky-900'>
       <article className='flex flex-col justify-center items-center gap-2'>
         <header className='max-w-[200px] text-center'>
-          <strong className='flex items-center justify-center h-[50px]'>{product.title}</strong>
-          <img className='w-[200px] aspect-square object-contain' src={product.thumbnail} alt={product.title} />
+          <strong className='flex items-center justify-center h-[50px]'>{title}</strong>
+          <img className='w-[200px] aspect-square object-contain' src={image} alt={product.title} />
         </header>
-        <footer className='flex gap-2'>
+        <footer className='flex gap-2 pb-2'>
           <span>
-            $ {product.price}
+            $ {price}
           </span>
-          <button className='bg-sky-400 py-1 px-2'>Add to Cart</button>
+          <button className='bg-sky-400 py-1 px-2 rounded'>Add to Cart</button>
         </footer>
       </article>
     </li>
@@ -18,13 +19,22 @@ export function ProductItem ({ product }) {
 }
 
 export function Products ({ products }) {
-  return (
-    <ul className='grid grid-cols-2 gap-2'>
-      {
-      products.map(item => (
-        <ProductItem key={item.id} product={item} />
-      ))
-    }
-    </ul>
-  )
+  const hasProducts = products?.length > 0
+  const mappedProducts = products.map(product => ({
+    title: product.title,
+    image: product.thumbnail,
+    price: product.price
+  }))
+
+  return hasProducts
+    ? (
+      <ul className='grid grid-cols-2 gap-2'>
+        {mappedProducts.map((item) => (
+          <ProductItem key={item.id} product={item} />
+        ))}
+      </ul>
+      )
+    : (
+      <p className='text-center'>No hay productos para mostrar</p>
+      )
 }
