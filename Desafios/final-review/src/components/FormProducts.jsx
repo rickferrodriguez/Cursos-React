d46f759743f9fe19facc22a2d78dddde71cfb09b
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-04-27 22:36:22 -0500

    jsconfig y name input text

--- Desafios/final-review/src/components/FormProducts.jsx
+++ Desafios/final-review/src/components/FormProducts.jsx
@@ -16,7 +16,7 @@ export function FormProducts () {
   return (
     <header>
       <form className='flex flex-col gap-2 items-center' onSubmit={handleSubmit}>
-        <input type='text' onChange={handleChange} id='name' className='text-gray-900' />
+        <input type='text' onChange={handleChange} value={search} name='search' className='text-gray-900' />
         <button type='submit' className='bg-sky-400 p-2 rounded w-[100px]'>Buscar</button>
       </form>
     </header>
