2f1ee0d4c53e560865d188b9fac9e93e0ff1b1f0
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-04-29 13:20:44 -0500

    useEffect form controlled by react

--- Desafios/final-review/src/components/FormProducts.jsx
+++ Desafios/final-review/src/components/FormProducts.jsx
@@ -1,8 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+function useSearch () {
+  const [search, setSearch] = useState('')
+  const [error, setError] = useState(null)
+  useEffect(() => {
+    if (search === '') {
+      setError('Ingrese un producto para buscar')
+      return
+    }
+
+    setError(null)
+  }, [search])
+
+  return { search, error, setSearch }
+}
 
 export function FormProducts () {
-  const [search, setSearch] = useState('')
-
+  const { search, error, setSearch } = useSearch()
   const handleSubmit = (event) => {
     event.preventDefault()
     console.log(search)
@@ -14,11 +28,23 @@ export function FormProducts () {
   }
 
   return (
-    <header>
-      <form className='flex flex-col gap-2 items-center' onSubmit={handleSubmit}>
-        <input type='text' onChange={handleChange} value={search} name='search' className='text-gray-900' />
-        <button type='submit' className='bg-sky-400 p-2 rounded w-[100px]'>Buscar</button>
+    <header className='flex flex-col items-center gap-3'>
+      <form
+        className='flex gap-2 items-center'
+        onSubmit={handleSubmit}
+      >
+        <input
+          type='text'
+          onChange={handleChange}
+          value={search}
+          name='search'
+          className='text-gray-900 px-2 rounded'
+        />
+        <button type='submit' className='bg-sky-400 p-2 rounded w-[100px]'>
+          Buscar
+        </button>
       </form>
+      <small className='text-red-500'>{error}</small>
     </header>
   )
 }
