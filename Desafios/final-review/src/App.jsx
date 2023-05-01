552358bbbccf16ab8c9ea7f48571523c94016a49
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-04-30 21:58:23 -0500

    reset test 2

--- Desafios/final-review/src/App.jsx
+++ Desafios/final-review/src/App.jsx
@@ -1,16 +1,6 @@
-import { Header } from './components/Header'
-import { Products } from './components/Products'
-import { useProducts } from './hooks/useProducts'
-
 function App () {
-  const { products, getProducts } = useProducts()
   return (
-    <>
-      <Header />
-      <main className='mt-3'>
-        <Products products={products} />
-      </main>
-    </>
+    <p>hola desde App</p>
   )
 }
 
--- Desafios/final-review/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import { FormProducts } from './FormProducts'
-export function Header () {
-  return (
-    <header className='flex flex-col gap-3 items-center'>
-      <h1 className='font-bold text-2xl text-center'>Product Cart</h1>
-      <FormProducts />
-    </header>
-  )
-}
