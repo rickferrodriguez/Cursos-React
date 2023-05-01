fd76a9e0f13a911d21240d635a4d3f64871f2848
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-05-01 16:17:08 -0500

    input para buscar la película

--- Desafios/final-review/src/App.jsx
+++ Desafios/final-review/src/App.jsx
@@ -1,6 +1,14 @@
 function App () {
   return (
-    <p>hola desde App</p>
+    <>
+      <header className='flex flex-col gap-4 items-center'>
+        <h1 className='text-4xl text-center text-sky-400 font-bold'>Buscador de Películas</h1>
+        <form className='flex gap-3'>
+          <input className='rounded' type='text' id='movie-name' />
+          <button type='submit'>Buscar</button>
+        </form>
+      </header>
+    </>
   )
 }
 
