2f9a3d3360cb6db50bde5053bfec4f72d91c4201
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-05-03 13:28:28 -0500

    hook search

--- Desafios/movies-review/src/App.jsx
+++ Desafios/movies-review/src/App.jsx
@@ -1,16 +1,30 @@
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
+import { useSearch } from './hooks/useSearch'
 
 export function App () {
   const { movies } = useMovies()
+  const { search, error, setSearch } = useSearch()
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(search)
+  }
+
+  const handleChange = (event) => {
+    const newSearch = event.target.value
+    setSearch(newSearch)
+  }
+
   return (
     <section className='grid gap-5'>
       <header className='grid gap-4 justify-center'>
         <h1 className='font-bold text-sky-400 text-center text-4xl'>Movie Finder</h1>
-        <form className='flex gap-3'>
-          <input className='px-2 rounded' type='text' id='name' name='name' placeholder='Hulk, Avengers, Harry Potter' />
-          <button className='bg-sky-400 px-2 py-1 rounded'>Buscar</button>
+        <form className='flex gap-3' onSubmit={handleSubmit}>
+          <input className='px-2 rounded text-gray-900' type='text' onChange={handleChange} value={search} id='name' name='name' placeholder='Hulk, Avengers, Harry Potter' />
+          <button type='submit' className='bg-sky-400 px-2 py-1 rounded'>Buscar</button>
         </form>
+        <small className='text-red-500 text-center'>{error}</small>
       </header>
 
       <Movies movies={movies} />
---
+++ Desafios/movies-review/src/hooks/useSearch.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useRef, useState } from 'react'
+
+export function useSearch () {
+  const [search, setSearch] = useState('')
+  const [error, setError] = useState(null)
+  const firstTimeInput = useRef(true)
+
+  useEffect(() => {
+    if (firstTimeInput.current) {
+      firstTimeInput.current = search === ''
+      return
+    }
+
+    if (search === '') {
+      setError('Please enter a movie to search')
+    }
+  }, [search])
+
+  return { search, setSearch, error }
+}
