f7bf861f58fbcaa77ab96e32d01a69b174678725
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-05-04 14:52:48 -0500

    list of products

--- Desafios/carrito-review/src/App.jsx
+++ Desafios/carrito-review/src/App.jsx
@@ -1,8 +1,12 @@
-import './App.css'
+import { Products } from './components/Products'
+import { products as initialProducts } from './mocks/products.json'
 
 function App () {
   return (
-    <p>hola desde App</p>
+    <>
+      <p>hola desde App</p>
+      <Products products={initialProducts} />
+    </>
   )
 }
 
---
+++ Desafios/carrito-review/src/components/Products.jsx
@@ -0,0 +1,35 @@
+export function ItemProduct ({ item }) {
+  const { title, description, thumbnail, price } = item
+  return (
+    <li className='flex flex-col gap-2'>
+      <strong className='text-center w-full text-xl'>{title}</strong>
+      <img className='w-[300px] aspect-[4/3] object-contain self-center' src={thumbnail} alt={title} />
+      <p className='line-clamp-3'>{description}</p>
+      <section className='flex justify-between'>
+        <span className='font-bold text-2xl'>$ {price}<small>.00</small></span>
+        <button className='bg-sky-600 px-2 rounded w-[200px]'>Add to Cart</button>
+      </section>
+    </li>
+  )
+}
+
+export function Products ({ products }) {
+  const hasProducts = products?.length > 0
+  return (
+    <main className='flex justify-center'>
+      {
+      hasProducts
+        ? (
+          <ul className='w-[320px] grid grid-cols-1 gap-6'>
+            {
+          products.map(item => (
+            <ItemProduct key={item.id} item={item} />
+          ))
+        }
+          </ul>
+          )
+        : <p>no hay products</p>
+    }
+    </main>
+  )
+}
