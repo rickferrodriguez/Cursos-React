9e95c097daf3723dea8fcc5bed913fd7f09f51a4
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-05-08 15:17:13 -0500

    useId

--- Desafios/carrito-review/src/components/Filters.jsx
+++ Desafios/carrito-review/src/components/Filters.jsx
@@ -1,8 +1,10 @@
-import { useContext } from 'react'
+import { useContext, useId } from 'react'
 import { FilterContext } from '../contexts/filters'
 
 export function Filters () {
   const { filters, setFilters } = useContext(FilterContext)
+  const minPriceFilterId = useId()
+  const categoryFilterId = useId()
 
   const handleMinPrice = (event) => {
     const newPrice = event.target.value
@@ -23,14 +25,26 @@ export function Filters () {
   return (
     <section className='flex justify-evenly'>
       <div className='flex gap-2'>
-        <label htmlFor='price'>Price</label>
-        <input type='range' id='price' min='0' max='1000' value={filters.minPrice} onChange={handleMinPrice} />
+        <label htmlFor={minPriceFilterId}>Price</label>
+        <input
+          type='range'
+          id={minPriceFilterId}
+          min='0'
+          max='1000'
+          value={filters.minPrice}
+          onChange={handleMinPrice}
+        />
         <span className='w-[50px]'>{filters.minPrice}</span>
       </div>
 
       <div className='flex gap-3'>
-        <label htmlFor='category'>Category</label>
-        <select className='text-gray-900 rounded px-1' onChange={handleCategory} name='category' id='category'>
+        <label htmlFor={categoryFilterId}>Category</label>
+        <select
+          className='text-gray-900 rounded px-1'
+          onChange={handleCategory}
+          name='category'
+          id={categoryFilterId}
+        >
           <option value='all'>All</option>
           <option value='laptops'>Laptops</option>
           <option value='smartphones'>Smartphones</option>
