11361bc3d922f41830ddd83a39676e1a57359c1a
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-05-08 22:26:57 -0500

    styles fixing add to cart remove from cart

--- Desafios/carrito-review/src/components/Products.jsx
+++ Desafios/carrito-review/src/components/Products.jsx
@@ -9,10 +9,10 @@ export function ItemProduct ({
 }) {
   const { title, description, thumbnail, price } = item
   return (
-    <li className='flex flex-col gap-2'>
+    <li className='flex flex-col gap-2 p-3 bg-slate-800 rounded'>
       <strong className='text-center w-full text-xl'>{title}</strong>
       <img
-        className='w-[300px] aspect-[4/3] object-contain self-center'
+        className='w-[300px] aspect-auto object-fit self-center rounded overflow-hidden'
         src={thumbnail}
         alt={title}
       />
@@ -25,8 +25,8 @@ export function ItemProduct ({
         <button
           onClick={isProductInCart ? removeFromCart : addToCart}
           className={`font-bold ${
-            isProductInCart ? 'bg-red-500' : 'bg-sky-600'
-          } px-2 rounded w-[200px] flex justify-evenly items-center`}
+            isProductInCart ? 'bg-red-600 hover:bg-red-800 flex flex-col justify-center gap-1 py-1' : 'bg-sky-600 flex justify-evenly hover:bg-sky-800'
+          } rounded w-[160px]  items-center `}
         >
           {isProductInCart ? 'Remove From Cart' : 'Add to Cart'}
           <span>
@@ -45,10 +45,10 @@ export function Products ({ products }) {
   }
   const hasProducts = products?.length > 0
   return (
-    <main className='flex justify-center'>
+    <main className='flex justify-center mt-[40px]'>
       {hasProducts
         ? (
-          <ul className='w-[320px] grid grid-cols-1 gap-6'>
+          <ul className='w-[310px] grid grid-cols-1 gap-6'>
             {products.map((item) => {
               const isProductInCart = checkProductInCart(item)
               return (
