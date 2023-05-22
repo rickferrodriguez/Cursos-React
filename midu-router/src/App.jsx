f2479f46226903e5e5e7dc784af468fe74a051d8
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-05-22 17:59:12 -0500

    reset midu router

--- midu-router/src/App.jsx
+++ midu-router/src/App.jsx
@@ -1,30 +1,10 @@
-import { useEffect, useState } from 'react'
-import { EVENTS } from './constants'
-import About from './pages/About'
-import Home from './pages/Home'
 
 function App () {
   // contiene la ruta donde se encuentra actualmente la página
-  const [currentPath, setCurrentPath] = useState(window.location.pathname)
-
-  useEffect(() => {
-    const onLocationChange = () => {
-      setCurrentPath(window.location.pathname)
-    }
-
-    window.addEventListener(EVENTS.PUSHSTATE, onLocationChange)
-    window.addEventListener(EVENTS.POPSTATE, onLocationChange)
-
-    return () => {
-      window.removeEventListener(EVENTS.PUSHSTATE, onLocationChange)
-      window.addEventListener(EVENTS.POPSTATE, onLocationChange)
-    }
-  }, [])
 
   return (
     <main>
-      {currentPath === '/' && <Home />}
-      {currentPath === '/about' && <About />}
+      <p>main</p>
     </main>
   )
 }
