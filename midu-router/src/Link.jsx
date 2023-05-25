aeb5321355510f62ab00abcd2a759915e8b1e68b
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-05-25 15:27:31 -0500

    more events for the link button

--- midu-router/src/Link.jsx
+++ midu-router/src/Link.jsx
@@ -8,8 +8,14 @@ export function navigate (href) {
 
 export function Link ({ to, target, ...props }) {
   const handleClick = (event) => {
-    event.preventDefault()
-    navigate(to)
+    const isMainEvent = event.button === 0
+    const isModifiedEvent = event.metaKey || event.ctrlKey || event.shiftKey || event.altKey
+    const isTargetEvent = target === undefined || target === '_self'
+
+    if (isMainEvent && isTargetEvent && !isModifiedEvent) {
+      event.preventDefault()
+      navigate(to)
+    }
   }
 
   return <a onClick={handleClick} href={to} target={target} {...props} />
