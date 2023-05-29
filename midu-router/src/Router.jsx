4edd5406b081258d782e553bf5dd6919d6c458ce
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-05-29 15:08:42 -0500

    reset again children is not define wtf error

--- midu-router/src/Router.jsx
+++ midu-router/src/Router.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react'
+import { match } from 'path-to-regexp'
+import { Children, useEffect, useState } from 'react'
 import { EVENTS } from './constants'
 
 export function Router ({ routes = [], defaultComponent: DefaultComponent = () => null }) {
@@ -18,7 +19,26 @@ export function Router ({ routes = [], defaultComponent: DefaultComponent = () =
     }
   }, [])
 
-  const Page = routes.find(({ path }) => path === actualPath)?.Component
+  let routeParams = {}
 
-  return Page ? <Page /> : <DefaultComponent />
+  const routesFromChildren = Children.map(children, () => {
+
+  })
+
+  const routesToUse = routes.concat(routesFromChildren)
+
+  const Page = routesToUse.find(({ path }) => {
+    if (path === actualPath) return true
+
+    const matcherUrl = match(path, { decode: decodeURIComponent })
+    const mached = matcherUrl(actualPath)
+    if (!mached) return false
+
+    routeParams = mached.params
+    return true
+  })?.Component
+
+  return Page
+    ? <Page routeParams={routeParams} />
+    : <DefaultComponent routeParams={routeParams} />
 }
