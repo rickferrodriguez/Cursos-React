5e2742b20cf99eb6f29236223ab3997951c12ecc
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-05-30 15:36:50 -0500

    another events for the link component

--- rick-router/src/Link.jsx
+++ rick-router/src/Link.jsx
@@ -8,8 +8,14 @@ export function navigate (href) {
 
 export function Link ({ to, target, ...props }) {
   const handleClick = (event) => {
-    event.preventDefault()
-    navigate(to)
+    const primaryEvent = event.button === 0
+    const isModifiedEvent = event.shiftKey || event.ctrlKey || event.metaKey || event.altKey
+    const isThisTarget = event.target === undefined || event.target === '_self'
+
+    if (primaryEvent && isThisTarget && !isModifiedEvent) {
+      event.preventDefault()
+      navigate(to)
+    }
   }
   return <a onClick={handleClick} href={to} target={target} {...props} />
 }
