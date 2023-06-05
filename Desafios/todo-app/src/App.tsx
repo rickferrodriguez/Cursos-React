58dda10d476969a4d7817b4b285c5c59d87ed424
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-06-05 10:59:10 -0500

    reset typescript react class

--- Desafios/todo-app/src/App.tsx
+++ Desafios/todo-app/src/App.tsx
@@ -1,26 +1,9 @@
-import { useState } from 'react'
-import { Todos } from './components/Todos'
-
-const mockTodos = [
-  {
-    id: '2',
-    title: 'todo 2',
-    completed: false
-  },
-  {
-    id: '3',
-    title: 'todo 3',
-    completed: false
-
-  }
-]
 
 const App = (): JSX.Element => {
-  const [todos, setTodos] = useState(mockTodos)
   return (
     <>
-    <h1>Todo App Typescript</h1>
-    <Todos todos={todos} />
+      <h1>Todo TypeScript App</h1>
+      <p>Hola desde el Reset de Todo App</p>
     </>
   )
 }
