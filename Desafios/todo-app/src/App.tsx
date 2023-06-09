8a171f8df38b9a34c03e9028ab54dd7eb5242187
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-06-09 14:48:23 -0500

    React Typescript todo reset

--- Desafios/todo-app/src/App.tsx
+++ Desafios/todo-app/src/App.tsx
@@ -1,38 +1,5 @@
-import { useState } from 'react'
-import { Todos } from './components/Todos'
-
-const mockTodos = [
-  {
-    id: '1',
-    title: 'Ver el video de retransmisión de Midu',
-    completed: true
-  },
-  {
-    id: '2',
-    title: 'Sacar el todo app with typescript',
-    completed: false
-  },
-  {
-    id: '3',
-    title: 'Ver la clase de typescript de midudev',
-    completed: false
-  }
-]
-
 const App = (): JSX.Element => {
-  const [todos, setTodos] = useState(mockTodos)
-
-  const handleRemove = (id: string): void => {
-    const newTodos = todos.filter((todo) => todo.id !== id)
-    setTodos(newTodos)
-  }
-
-  return (
-    <div className='todoapp'>
-      <h1>Todo App</h1>
-      <Todos onRemoveTodo={handleRemove} todos={todos} />
-    </div>
-  )
+  return <p>Reset Todo App</p>
 }
 
 export default App
