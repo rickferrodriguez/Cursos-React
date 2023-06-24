3e592cd6f48f0319507ed4c8c50c3edb17fbd2a3
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-06-24 17:41:22 -0500

    complete a Todo

--- Desafios/latest/src/App.tsx
+++ Desafios/latest/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Todos } from './components/Todos'
-import { type TodoId } from './types'
+import { type Todo as TodoType, type TodoId } from './types'
 
 const mocksTodos = [
   {
@@ -19,13 +19,28 @@ const App = (): JSX.Element => {
   const [todos, setTodos] = useState(mocksTodos)
 
   const handleRemove = ({ id }: TodoId): void => {
-    const newTodos = todos.filter(todo => todo.id !== id)
+    const newTodos = todos.filter((todo) => todo.id !== id)
+    setTodos(newTodos)
+  }
+
+  const handleCompleted = ({ id, completed }: Pick<TodoType, 'id' | 'completed'>): void => {
+    const newTodos = todos.map(todo => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed
+        }
+      }
+
+      return todo
+    })
+
     setTodos(newTodos)
   }
   return (
     <div className='todoapp'>
       <h1>Todo App</h1>
-      <Todos todos={todos} onRemoveTodos={handleRemove}/>
+      <Todos todos={todos} onRemoveTodos={handleRemove} onToggleCompleteTodo={handleCompleted} />
     </div>
   )
 }
--- Desafios/latest/src/components/Todo.tsx
+++ Desafios/latest/src/components/Todo.tsx
@@ -2,17 +2,20 @@ import { type TodoId, type Todo as TodoType } from '../types'
 
 interface Props extends TodoType {
   onRemoveTodos: ({ id }: TodoId) => void
+  onToggleCompleteTodo: ({ id, completed }: Pick<TodoType, 'id' | 'completed'>) => void
 }
 
-export const Todo: React.FC<Props> = ({
-  id,
-  title,
-  completed,
-  onRemoveTodos
-}) => {
+export const Todo: React.FC<Props> = ({ id, title, completed, onRemoveTodos, onToggleCompleteTodo }) => {
   return (
     <div className='view'>
-      <input type='checkbox' className='toggle' checked={completed} />
+      <input
+        type='checkbox'
+        className='toggle'
+        checked={completed}
+        onChange={(event) => {
+          onToggleCompleteTodo({ id, completed: event.target.checked })
+        }}
+      />
       <label>{title}</label>
       <button
         className='destroy'
