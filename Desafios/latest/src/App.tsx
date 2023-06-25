a50753c7b03baad36d7db48a3daa1a1b979ca13b
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-06-25 18:40:19 -0500

    list all of the todos

--- Desafios/latest/src/App.tsx
+++ Desafios/latest/src/App.tsx
@@ -1,7 +1,26 @@
+import { useState } from 'react'
+import { Todos } from './components/Todos'
+
+const mocksTodos = [
+  {
+    id: '1',
+    title: 'todo numero 1',
+    completed: false
+  },
+  {
+    id: '2',
+    title: 'todo numero 2',
+    completed: false
+  }
+]
 
 const App = (): JSX.Element => {
+  const [todos, setTodos] = useState(mocksTodos)
   return (
-    <h1>Reset todo app</h1>
+    <div className='todoapp'>
+      <h1>todo app</h1>
+      <Todos todos={todos} />
+    </div>
   )
 }
 
