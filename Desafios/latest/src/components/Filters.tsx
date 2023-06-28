4612ad0873c3c912c1c6c170077295bf80b014bc
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-06-27 23:57:01 -0500

    typescript reset

--- Desafios/latest/src/components/Filters.tsx
+++
@@ -1,7 +0,0 @@
-export const Filters: React.FC = ({ filtersSelected, onFiltersChange }) => {
-  return (
-    <ul className="filters">
-
-    </ul>
-  )
-}
--- Desafios/latest/src/consts.ts
+++
@@ -1,5 +0,0 @@
-export const TODO_FILTERS = {
-  ALL: 'all',
-  ACTIVE: 'active',
-  COMPLETED: 'completed'
-} as const
