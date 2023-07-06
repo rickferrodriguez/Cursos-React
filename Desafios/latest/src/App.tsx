798ddbab437009997be31b6636475389991f45e8
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-07-06 15:12:40 -0500

    set a todo as completed

--- Desafios/latest/src/App.tsx
+++ Desafios/latest/src/App.tsx
@@ -1,19 +1,39 @@
 import { useState } from 'react'
 import { mocksTodos } from './mocks'
 import { Todos } from './components/Todos'
-import { type TodoId } from './types'
+import { type Todo, type TodoId } from './types'
 
 const App = (): JSX.Element => {
   const [todos, setTodos] = useState(mocksTodos)
 
   const handleRemoveTodo = ({ id }: TodoId): void => {
-    const newTodos = todos.filter(todo => todo.id !== id)
+    const newTodos = todos.filter((todo) => todo.id !== id)
+    setTodos(newTodos)
+  }
+
+  const handleCompleteTodo = ({
+    id,
+    completed
+  }: Pick<Todo, 'id' | 'completed'>): void => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed
+        }
+      }
+      return todo
+    })
     setTodos(newTodos)
   }
   return (
     <div className='todoapp'>
-    <h1>Reset todo</h1>
-    <Todos todos={todos} onRemoveTodos={handleRemoveTodo}/>
+      <h1>Reset todo</h1>
+      <Todos
+        todos={todos}
+        onRemoveTodos={handleRemoveTodo}
+        onCompleteTodo={handleCompleteTodo}
+      />
     </div>
   )
 }
--- Desafios/latest/src/components/Todos.tsx
+++ Desafios/latest/src/components/Todos.tsx
@@ -1,23 +1,35 @@
-import { type TodoId, type TodoList } from '../types'
+import { type Todo as TodoType, type TodoId, type TodoList } from '../types'
 import { Todo } from './Todo'
 
 interface Props {
   todos: TodoList
   onRemoveTodos: ({ id }: TodoId) => void
+  onCompleteTodo: ({
+    id,
+    completed
+  }: Pick<TodoType, 'id' | 'completed'>) => void
 }
 
-export const Todos: React.FC<Props> = ({ todos, onRemoveTodos }) => {
+export const Todos: React.FC<Props> = ({
+  todos,
+  onRemoveTodos,
+  onCompleteTodo
+}) => {
   return (
-  <ul className='todo-list'>
-    {
-      todos.map(todo => (
+    <ul className='todo-list'>
+      {todos.map((todo) => (
         <li key={todo.id} className={`${todo.completed ? 'completed' : ''}`}>
-        {
-          <Todo id={todo.id} title={todo.title} completed={todo.completed} onRemoveTodos={onRemoveTodos} />
-        }
+          {
+            <Todo
+              id={todo.id}
+              title={todo.title}
+              completed={todo.completed}
+              onRemoveTodos={onRemoveTodos}
+              onCompleteTodo={onCompleteTodo}
+            />
+          }
         </li>
-      ))
-    }
-  </ul>
+      ))}
+    </ul>
   )
 }
