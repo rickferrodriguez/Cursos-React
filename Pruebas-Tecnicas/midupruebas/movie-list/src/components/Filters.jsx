7dde38ed60e52f7b971cdf1bbed98bcea32e27f2
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-10-04 15:08:20 -0500

    header and filters

---
+++ Pruebas-Tecnicas/midupruebas/movie-list/src/components/Filters.jsx
@@ -0,0 +1,8 @@
+export function Filters() {
+  return (
+    <section className='flex gap-3 text-gray-300'>
+      <label htmlFor=''>Search for name</label>
+      <input type='text' />
+    </section>
+  )
+}
---
+++ Pruebas-Tecnicas/midupruebas/movie-list/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import { Filters } from './Filters'
+
+export function Header() {
+  return (
+    <header>
+      <h1 className='font-bold text-xl text-gray-100'>List of Movies</h1>
+      <Filters />
+    </header>
+  )
+}
