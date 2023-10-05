92e079e6fc0d710b571dc0b4e3d844e1d86c3932
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-10-05 15:05:54 -0500

    check readme

--- Pruebas-Tecnicas/midupruebas/movie-list/src/components/Filters.jsx
+++ Pruebas-Tecnicas/midupruebas/movie-list/src/components/Filters.jsx
@@ -1,8 +1,9 @@
 export function Filters() {
   return (
-    <section className='flex gap-3 text-gray-300'>
+    <form className='flex gap-3 text-gray-300'>
       <label htmlFor=''>Search for name</label>
       <input type='text' />
-    </section>
+      <button className='bg-slate-700 px-2 py-1 rounded-md'>Search</button>
+    </form>
   )
 }
--- Pruebas-Tecnicas/midupruebas/movie-list/src/components/Header.jsx
+++ Pruebas-Tecnicas/midupruebas/movie-list/src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Filters } from './Filters'
 
 export function Header() {
   return (
-    <header>
+    <header className='flex flex-col gap-4 items-center'>
       <h1 className='font-bold text-xl text-gray-100'>List of Movies</h1>
       <Filters />
     </header>
