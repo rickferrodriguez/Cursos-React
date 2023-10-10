9890f013d8d6d5d971ce6d3e38f4252bf5ef1bb5
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-10-10 15:41:17 -0500

    list all of the movies

--- Pruebas-Tecnicas/midupruebas/movie-list/src/App.jsx
+++ Pruebas-Tecnicas/midupruebas/movie-list/src/App.jsx
@@ -1,8 +1,22 @@
+import { useState } from 'react'
 import './App.css'
 import { Header } from './components/Header'
+import { Search as ListOfMovies } from './mocks/withResults.json'
 
 function App() {
-  return <Header />
+  const [movies, setMovies] = useState(ListOfMovies)
+  return (
+    <main>
+      <Header />
+      <div>
+        {movies.map((movie) => (
+          <article key={movie.imdbID}>
+            <h1>{movie.Title}</h1>
+          </article>
+        ))}
+      </div>
+    </main>
+  )
 }
 
 export default App
