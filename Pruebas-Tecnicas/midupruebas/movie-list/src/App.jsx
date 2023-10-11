a2e132d74f46d30c1dcbe4b1c10f54cf6be1d479
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-10-11 12:01:47 -0500

    list all of the movies

--- Pruebas-Tecnicas/midupruebas/movie-list/src/App.jsx
+++ Pruebas-Tecnicas/midupruebas/movie-list/src/App.jsx
@@ -1,17 +1,22 @@
 import { useState } from 'react'
-import './App.css'
 import { Header } from './components/Header'
 import { Search as ListOfMovies } from './mocks/withResults.json'
 
 function App() {
   const [movies, setMovies] = useState(ListOfMovies)
   return (
-    <main>
+    <main className='flex flex-col gap-4'>
       <Header />
-      <div>
+      <div className='grid grid-cols-3 gap-3'>
         {movies.map((movie) => (
-          <article key={movie.imdbID}>
-            <h1>{movie.Title}</h1>
+          <article key={movie.imdbID} className='w-auto'>
+            <img
+              className='w-auto  aspect-[3/4] object-contain'
+              src={movie.Poster}
+              alt={movie.Title}
+            />
+            <section>{movie.Type}</section>
+            <h1 className='text-gray-400'>{movie.Title}</h1>
           </article>
         ))}
       </div>
