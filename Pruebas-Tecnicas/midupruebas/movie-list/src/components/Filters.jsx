844bf0926432f25cbd5819dc0c358fe6282ed66f
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-10-13 21:06:00 -0500

    readme

--- Pruebas-Tecnicas/midupruebas/movie-list/src/components/Filters.jsx
+++ Pruebas-Tecnicas/midupruebas/movie-list/src/components/Filters.jsx
@@ -1,7 +1,7 @@
 export function Filters() {
   return (
     <form className='flex gap-3 text-gray-300'>
-      <label htmlFor=''>Search for name</label>
+      <label htmlFor='searchMovie'>Search for name</label>
       <input type='text' />
       <button className='bg-slate-700 px-2 py-1 rounded-md'>Search</button>
     </form>
