401dc1d0cf25e2b2ac8b9f834fc1e8c06dda4dae
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-10-19 14:30:28 -0500

    reset primeros principios

--- Pruebas-Tecnicas/midupruebas/movie-list/src/App.jsx
+++ Pruebas-Tecnicas/midupruebas/movie-list/src/App.jsx
@@ -1,46 +1,8 @@
-import { Header } from './components/Header'
-import { Movies } from './components/Movies'
-import { useMovies } from './hooks/useMovies'
-import { useEffect } from 'react'
-import { useState } from 'react'
-
-function useSearch() {
-  const [search, setSearch] = useState('')
-  const [error, setError] = useState(null)
-
-  useEffect(() => {
-    if (search === '') {
-      setError('Ingrese la película ha buscar')
-      return
-    }
-
-    setError(null)
-  }, [search])
-  return { search, setSearch, error }
-}
-
 function App() {
-  const { movies } = useMovies()
-  const { search, error, setSearch } = useSearch()
-
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    console.log(search)
-  }
-
-  const handleSearch = (event) => {
-    const currentValue = event.target.value
-    setSearch(currentValue)
-  }
   return (
-    <main className='flex flex-col gap-4'>
-      <Header
-        handleSearch={handleSearch}
-        handleSubmit={handleSubmit}
-        error={error}
-        search={search}
-      />
-      <Movies movies={movies} />
+    <main>
+      <h1>Buscador de Películas</h1>
+      <p>Sistema de primeros priencipios para estudiar de nuevo</p>
     </main>
   )
 }
