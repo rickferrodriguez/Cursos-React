9ffb7087f13350c2b1259859cfd04b300c284829
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-10-23 14:58:03 -0500

    movie component

--- Pruebas-Tecnicas/midupruebas/movie-list/src/App.jsx
+++ Pruebas-Tecnicas/midupruebas/movie-list/src/App.jsx
@@ -1,11 +1,9 @@
-import { MovieIcon, SearchIcon, SeriesIcon } from './components/Icons'
+import { SearchIcon } from './components/Icons'
+import { Movies } from './components/Movies'
 import moviesResult from './mocks/withResults.json'
 
 function App() {
   const movies = moviesResult.Search
-  const typeOfData = (type) => {
-    return type === 'movie' ? <MovieIcon /> : <SeriesIcon />
-  }
   return (
     <>
       <header className='flex flex-col items-center gap-4 mb-9'>
@@ -26,29 +24,7 @@ function App() {
         </form>
       </header>
       <main>
-        <ul className='grid lg:grid-cols-[repeat(5,_1fr)] grid-cols-[repeat(3,_1fr)] gap-3 text-gray-400'>
-          {movies.map((movie) => (
-            <li key={movie.imdbID}>
-              <header className='bg-slate-900 rounded-lg overflow-hidden'>
-                <img
-                  className='aspect-[2/3] object-fill '
-                  src={movie.Poster}
-                  alt={movie.Title}
-                />
-                <section className='flex py-1 justify-between text-sm px-1'>
-                  <div className='rounded-md px-1 flex items-center'>
-                    <span>{movie.Year}</span>
-                  </div>
-                  <div className='flex bg-sky-950 rounded-md font-medium'>
-                    <i className='scale-[65%]'>{typeOfData(movie.Type)}</i>
-                    <span className='mr-1'>{movie.Type}</span>
-                  </div>
-                </section>
-              </header>
-              <h3 className='line-clamp-2'>{movie.Title}</h3>
-            </li>
-          ))}
-        </ul>
+        <Movies movies={movies} />
       </main>
     </>
   )
---
+++ Pruebas-Tecnicas/midupruebas/movie-list/src/components/Movies.jsx
@@ -0,0 +1,47 @@
+import { MovieIcon, SeriesIcon } from '../components/Icons.jsx'
+export function ListOfMovies({ movies }) {
+  const typeOfData = (type) => {
+    return type === 'movie' ? <MovieIcon /> : <SeriesIcon />
+  }
+  return (
+    <ul className='grid lg:grid-cols-[repeat(5,_1fr)] grid-cols-[repeat(3,_1fr)] gap-3 text-gray-400'>
+      {movies.map((movie) => (
+        <li key={movie.imdbID}>
+          <header className='bg-slate-900 rounded-lg overflow-hidden'>
+            <img
+              className='aspect-[2/3] object-fill '
+              src={movie.Poster}
+              alt={movie.Title}
+            />
+            <section className='flex py-1 justify-between text-sm px-1'>
+              <div className='rounded-md px-1 flex items-center'>
+                <span>{movie.Year}</span>
+              </div>
+              <div className='flex bg-sky-950 rounded-md font-medium'>
+                <i className='scale-[65%]'>{typeOfData(movie.Type)}</i>
+                <span className='mr-1'>{movie.Type}</span>
+              </div>
+            </section>
+          </header>
+          <h3 className='line-clamp-2'>{movie.Title}</h3>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+function NoResponseMovies() {
+  return <p>No existen películas para su búsqueda</p>
+}
+
+export function Movies({ movies }) {
+  return (
+    <>
+      {movies.length > 0 ? (
+        <ListOfMovies movies={movies} />
+      ) : (
+        <NoResponseMovies />
+      )}
+    </>
+  )
+}
